fb2f0ea89f7f9ad983d0dffc576f918a21a42bbb
Avinash Dhawan <60123725+avinashdhawan@users.noreply.github.com>
2020-10-31 14:37:06 -0400

    file cleaned converted back to csv

---
+++ weather2020.sql
@@ -0,0 +1,48 @@
+DROP TABLE weather2019;
+DROP TABLE weather2020;
+
+CREATE TABLE weather2019 (
+	long DECIMAL(5,2),
+	lat DECIMAL(5,2),
+	station VARCHAR(20),
+	date VARCHAR(10) NOT NULL,
+    year VARCHAR   NOT NULL,
+    month INT NOT NULL,
+	day INT NOT NULL,
+	heatdays DECIMAL(4,1),
+	cooldays DECIMAL(4,1),
+	maxtemp DECIMAL(4,1),
+	mintemp DECIMAL(4,1),
+	meantemp DECIMAL(4,1),
+	precip	DECIMAL(4,1),
+	rain	DECIMAL(4,1),
+	snowfall DECIMAL(4,1),
+	snowongrd	DECIMAL(4,1),
+	winddir DECIMAL(4,1),
+	windspeed DECIMAL(4,1)
+	);
+	
+CREATE TABLE weather2020 (
+	long DECIMAL(5,2),
+	lat DECIMAL(5,2),
+	station VARCHAR(20),
+	date VARCHAR(10) NOT NULL,
+    year VARCHAR   NOT NULL,
+    month INT NOT NULL,
+	day INT NOT NULL,
+	heatdays DECIMAL(4,1),
+	cooldays DECIMAL(4,1),
+	maxtemp DECIMAL(4,1),
+	mintemp DECIMAL(4,1),
+	meantemp DECIMAL(4,1),
+	precip	DECIMAL(4,1),
+	rain	DECIMAL(4,1),
+	snowfall DECIMAL(4,1),
+	snowongrd	DECIMAL(4,1),
+	winddir DECIMAL(4,1),
+	windspeed DECIMAL(4,1)
+	);
+	
+SELECT * FROM weather2019;
+SELECT * FROM weather2020;
+
